0d06bb269b00224b7dd38fc4deee12abeb23cc7e
jaimie <moore.jaimie@gmail.com>
2019-02-20 17:23:48 -0500

    arrow function modification

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const app = {};
 app.apiKeyWeather = '8f6dab90acf14f9da11221736191902'
 app.apiUrlWeather = 'http://api.apixu.com/v1/current.json'
 
-app.init = function () {
+app.init = () => {
     app.getWeather(); //calling ajax request
 }
 
-app.getWeather = function (toronto) {
+app.getWeather = (toronto) => {
     $.ajax({
         url: app.apiUrlWeather,
         method: 'GET',
