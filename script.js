7a65eb73cbc6883ce4b94684795d4704eea15241
jaimie <moore.jaimie@gmail.com>
2019-02-20 19:24:26 -0500

    minor changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const app = {};
-app.apiKeyWeather = '8f6dab90acf14f9da11221736191902'
-app.apiUrlWeather = 'http://api.apixu.com/v1/current.json'
+app.apiKeyWeather = '8f6dab90acf14f9da11221736191902';
+app.apiUrlWeather = 'http://api.apixu.com/v1/current.json';
 
 app.init = () => {
     app.getWeather(); //calling ajax request
-}
+} //app.init ends here
 
 app.getWeather = (toronto) => {
     $.ajax({
@@ -12,15 +12,17 @@ app.getWeather = (toronto) => {
         method: 'GET',
         dataType: 'json',
         data: {
-            key: app.apiKeyWeather
+            key: app.apiKeyWeather,
             q: 'toronto'
         }
     }).then(function (response) { 
-        console.log(response)
+        // console.log(response.current)
+        console.log(response.current.feelslike_c)
+        console.log(response.current.condition.icon)
+        console.log(response.current.condition.text)
     });
-}
-
+} //app.getWeather ends here
 
 $(function () {
     app.init();
-})
+}) // doc ready ends here
