f2b7b9b4a1a2f026358cbbb32d22f5768124b936
Jeannette <jeannetteshui.ng@gmail.com>
2019-02-22 11:29:30 -0500

    delete previous etsydisplay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const app = {};
 app.apiKeyWeather = "8f6dab90acf14f9da11221736191902";
 app.apiUrlWeather = "http://api.apixu.com/v1/current.json";
-app.apiKeyEtsy = "rkcrycwnyei9ana141cxdioh";
+app.apiKeyEtsy = "xjgnf5lk3m3ruxemnnsz5knp";
 app.apiUrlEtsy = "https://openapi.etsy.com/v2/shops/polomocha/listings/active";
 app.userInput = ""; //empty var for user to enter city
 
@@ -25,7 +25,7 @@ app.getWeather = location => {
   });
 }; //app.getWeather ends here
 
-let weatherArray = []; 
+let weatherArray = [];
 app.displayWeather = function(data) {
   weatherArray.pop(); //remove content from weatherArray on each call to API (on additional submit)
   weatherArray.push(data);
@@ -36,7 +36,7 @@ app.displayWeather = function(data) {
                   current.location.name
                 }, the weather is ${current.current.condition.text} with current temperature of ${current.current.temp_c}, but it feels like ${current.current.feelslike_c} degrees celcius</h2>
             </div>`;
-  }); 
+  });
 
   $(".displayWeather").html("");
   $(".displayWeather").html(app.currentConditions);
@@ -44,35 +44,34 @@ app.displayWeather = function(data) {
 
 //error handling, if user does not input city
 app.emptyInput = function() {
-  if ($('#city').val() === '') {
+  if ($("#city").val() === "") {
     Swal.fire({
-      title: 'Error!',
-      text: 'Enter city',
-      type: 'error',
-      confirmButtonText: 'OK'
+      title: "Error!",
+      text: "Enter city",
+      type: "error",
+      confirmButtonText: "OK"
     });
-  };
+  }
 }; //app.emptyInput ends here
 
-app.handleEmptyInput = function () {
-  $("#submitButton").on("click", function () {
+app.handleEmptyInput = function() {
+  $("#submitButton").on("click", function() {
     app.emptyInput();
   });
-}; //event handler for on submit, specific to on submit  *** i think we need to rejig init and move the call app.callEtsyApiTwice outside of init, to make it global and then call it in init. is this proper name spaceing? question for helpcue *** 
+}; //event handler for on submit, specific to on submit  *** i think we need to rejig init and move the call app.callEtsyApiTwice outside of init, to make it global and then call it in init. is this proper name spaceing? question for helpcue ***
 
 // generate button is hidden on page load, show button on submit
-app.generateButton = function () {
-  $("#submitButton").on("click", function () {
-    $('#generate').show();
-  })
-}
+app.generateButton = function() {
+  $("#submitButton").on("click", function() {
+    $("#generate").show();
+  });
+};
 
 //app.getValue gets the value of the users input, sends it to app.getWeather
-app.getValue = function () {
+app.getValue = function() {
   app.location = $("#city").val();
   app.getWeather(app.location);
-}
-
+};
 
 //once receive temperature, it will compare it with 0
 //if above 0'C, return Etsy A (Tshirt,Short Pants)
@@ -97,26 +96,22 @@ app.callEtsyApiTwice = param => {
     let argItem = args.map((item, index) => {
       console.log(item);
       const i = Math.floor(Math.random() * item[0].results.length);
-      return item[0].results[i].MainImage.url_75x75;
+      return item[0].results[i].MainImage.url_fullxfull;
     });
     console.log(argItem);
     app.displayEtsy(argItem); //
   });
 }; //callEtsyApiTwice ends here
 
-
-
 app.displayEtsy = item => {
-  let itemDisplay = item.map(item => {
+  let itemDisplay = item.map((item, index) => {
     console.log(item);
-    return `<img src="${item}">`;
+    return `<img src="${item}" id="clothing-${index}">`;
   });
 
-  $(".displayTop").html("");
-  $(".displayTop").append(itemDisplay);
+  $(".displayOutfits").html("");
+  $(".displayOutfits").append(itemDisplay);
 };
-
-
 
 app.getEtsy = item => {
   //proxy
@@ -135,29 +130,15 @@ app.getEtsy = item => {
   });
 };
 
-let etsyArray = [];
-app.displayEtsy = data => {
-  etsyArray.pop();
-  etsyArray.push(data);
-  app.etsyImage = etsyArray.map(function(item) {
-    // let top = item[0].MainImage.url_fullxfull;
-    let top = item[0].MainImage.url_75x75;
-    let bottom = item[2].MainImage.url_75x75;
-
-    $(".displayTop").html("");
-    $(".displayTop").append(`<img src="${top}" />`);
-    $(".displayTop").append(`<img src="${bottom}" />`);
-  });
-};
-
 $("#generate").on("click", function() {
   //call api twice with top/bottom then return to DOM.
 });
 
 app.init = () => {
-  app.getWeather(); //calling 
+  app.getWeather(); //calling
   // app.handleEmptyInput();
-  $("#submitButton").on("click", function () { //proper name spacing?
+  $("#submitButton").on("click", function() {
+    //proper name spacing?
     app.handleEmptyInput(); //calling
     app.getValue();
     //displayWeather will show the API data according to userInput to DOM *** ask about namespacing
